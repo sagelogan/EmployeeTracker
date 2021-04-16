72d8a671d20833b1a80ab93bfac7577157f86b12
Logan Sage <lvdsage@gmail.com>
2021-04-16 12:30:02 -0600

    exporting missing functions

---
+++ assets/js/view.js
@@ -0,0 +1,9 @@
+const VIEW_ALL_EMPLOYEES = "View All Employees";
+const VIEW_ALL_DEPARTMENTS = "View All Departments";
+const VIEW_ALL_ROLE = "View all Roles";
+
+module.exports = [
+    VIEW_ALL_EMPLOYEES,
+    VIEW_ALL_ROLE,
+    VIEW_ALL_DEPARTMENTS,
+];
